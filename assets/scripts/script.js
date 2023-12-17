7985bcccd8c2f6f4f03339c739a00e239b32b399
Chris Stevenson <chrislstevenson@gmail.com>
2023-12-17 06:10:49 -0500

    Added current date to header

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -5,9 +7,13 @@ $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // function? How can DOM traversal be used to get the hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -15,9 +21,15 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+  // function to display the current date in the header
+  // called by the same timer that updates the scheduler
+  // returns date as string "MMM-DD" to allow one to determine if header needs updating
+  function displayDate() {
+    var now = dayjs();
+    $('#currentDay').text(now.format("dddd, MMMM D") + "th");
+    return now.format("MMM-DD");
+  }
+  // eventually will be called in a timer; delete the line below when that is added
+  displayDate();
+}); // end ready function
