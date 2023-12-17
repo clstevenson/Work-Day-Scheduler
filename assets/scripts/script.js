21fcb421795f7cafc4f75a83138d25bf5c03fa52
Chris Stevenson <chrislstevenson@gmail.com>
2023-12-17 15:29:49 -0500

    Scheduler is now functional
    - all the app updates on saving, getting, and displaying the data were connected

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,17 +4,14 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // global variable for current time
-  var now = dayjs();
-
   // function to display the current date in the header
-  // called by the same timer that updates the scheduler
+  // called by the same timer that updates the scheduler colors
   function displayDate() {
-    $('#currentDay').text(now.format("dddd, MMMM D") + "th");
+    $('#currentDay').text(dayjs().format("dddd, MMMM D") + "th");
   }
 
-  // initialize the displayed date and schedule when loaded
-  function updateDisplay() {
+  // function called by the timer
+  function updateBlockColors() {
     displayDate();  // put up the current date in header
     var currentHour = dayjs().hour();
     var blockHour;  // hour of the time block
@@ -41,22 +38,62 @@ $(function () {
     });
   }
 
-  // add a timer; how often should it be updated? Every second, every minute?
-  updateDisplay();
-  setInterval(updateDisplay, 1000);
+  // function to retrieve data from local storage
+  function getWorkday() {
+    if (localStorage.date === dayjs().format("YYYY-MM-DD")) {
+      return JSON.parse(localStorage.workday);
+    } else {
+      // assuming for a new day we clear the entries
+      localStorage.clear;
+      return [];
+    }
+  }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // function to update display of workday in window
+  function showWorkday(workday) {
+    $("textarea.description").each(function (index) {
+      var hour = +$(this).closest("div.time-block").attr("ID").split("-")[1];
+      // each entry has a leading space
+      if (workday[hour] != null || workday[hour] != undefined) {
+        $(this).val(" " + workday[hour]);
+      } else {
+        $(this).val(" ");
+      }
+    });
+  }
 
-  // also want to offer some visual feedback to the user when the data is saved, maybe
+  // function to write data to local storage
+  function saveWorkday(workday) {
+    localStorage.clear();
+    localStorage.date = dayjs().format("YYYY-MM-DD");
+    localStorage.workday = JSON.stringify(workday);
+  }
+
+  // update the date and the background-colors
+  updateBlockColors();
+  // add a timer to update colors as the day progresses
+  // (how often should it be updated? Every second, every minute?)
+  setInterval(updateBlockColors, 1000);
+
+  // update the text being displayed
+  showWorkday(getWorkday());
+
+  // TODO also want to offer some visual feedback to the user when the data is saved, maybe
   // a momentary change in the background-color of the icon along with a brief "saved" message
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  $("div.container-lg").click( function(evt) {
+    // only act if the save icon or its button was clicked. In both cases the class name contains "save"
+    if ($(evt.target).attr("class").includes("save")) {
+      // first get the current workday
+      var workday = getWorkday();
+      // now get the text in the textarea input (and its hour)
+      var timeBlockEl = $(evt.target.closest(".time-block"));
+      var hour = +timeBlockEl.attr("ID").split("-")[1];
+      var desc = timeBlockEl.find("textarea").val().trim();
+      workday[hour] = desc;
+      // now save to local storage
+      saveWorkday(workday);
+    }
+  }); // end click event listener
 
 }); // end ready function
